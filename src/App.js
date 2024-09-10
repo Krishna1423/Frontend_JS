682eb72a0dd4b5c1cc6e8f8c173da4c0559a647f
Krishna A S <krishnasaye23@gmail.com>
2024-09-09 23:19:48 -0400

    Lab 1-First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'; 
+import Greeter from './Greeter';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <h1>Welcome to my React App</h1>
+      <Greeter /> {/* Using Greeter component */}
+      </div>
   );
 }
 
